b719009c6088f16939b2ebfff7e06c2762f62496
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-08-01 14:02:43 -0500

    global and local variables

---
+++ app.js
@@ -0,0 +1,12 @@
+/****************************************************
+*              Global & Local Variables             *
+****************************************************/
+//var girl = " kelsey ";
+
+function spit() {
+  var girl = " kelsey ";
+  document.write(girl);
+}
+
+spit();
+document.write(girl);
